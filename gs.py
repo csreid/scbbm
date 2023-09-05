7ee49fd34e112509fcf9b3b785dc9c9e285cb090
Cameron Reid <csreid.cr@gmail.com>
2023-09-05 09:59:22 -0400

    Working training finally

--- gs.py
+++ gs.py
@@ -1,14 +1,16 @@
 import sys
 sys.path.append('/home/csreid/.pyenv/versions/3.10.12/lib/python3.10/site-packages')
-
 from pbp_dataset import PlayByPlayDataset
 import torch
+import pandas as pd
 from torch.nn import GRU, CrossEntropyLoss, Module, KLDivLoss, LSTM, Linear, functional as F, RNN, MSELoss, BCEWithLogitsLoss
 from torch.utils.data import DataLoader
 from torch.nn.utils.rnn import pad_sequence, pack_padded_sequence, pad_packed_sequence
-from torch.optim import Adam
+from torch.optim import Adam, SGD, RMSprop
 import matplotlib.pyplot as plt
 from IPython.display import clear_output
+from IPython import embed
+
 import numpy as np
 from tqdm import tqdm
 from torch.utils.tensorboard import SummaryWriter
@@ -25,65 +27,108 @@ def data_collate(samples):
 	plays_tens = pad_sequence(plays)
 	plays_tens = pack_padded_sequence(plays_tens, [len(p) for p in plays], enforce_sorted=False)
 
-	targets_tens = torch.stack(targets)
+	targets_tens = pad_sequence(targets)
 
-	return teams_tens, plays_tens, targets_tens.squeeze()
+	#targets_tens = torch.stack(targets_tens)
+	targets_tens = pack_padded_sequence(targets_tens, [len(p) for p in targets], enforce_sorted=False)
+
+	return teams_tens.unsqueeze(0), plays_tens, targets_tens#targets_tens.squeeze()
 
 
 class GameSimulator(Module):
-	def __init__(self, n_teams, n_plays, max_len):
+	def __init__(self, n_teams, n_plays):
 		super().__init__()
 		self._n_plays = n_plays
-		self._max_len = max_len
-
-		## With the "predict plays and the time they occurred" model, use a 
-		## 2-layer GRU/LSTM/whatever. The first layer should be used to predict the play,
-		## and the second layer should be used to predict the time it happened
 
 		self.team_input = Linear(n_teams, 1000)
 		self.play_input = Linear(n_plays, 1000)
-		self.recurrent = LSTM(1000, 1000, batch_first=True)
+		self.recurrent = GRU(1000, 1000)
 		self.output = Linear(1000, n_plays)
 
 	def forward(self, teams, plays):
-		team_out = self.team_input(teams)
-		play_out = self.play_input(plays)
+		team_out = F.tanh(self.team_input(teams))
+		team_out, h = self.recurrent(team_out)
 
-		out, (h, c) = self.recurrent(team_out)
-		out, (h, c) = self.recurrent(out, (h, c))
+		play_out = F.tanh(self.play_input(plays))
+		out, _ = self.recurrent(play_out, h)
+		#out = out[-1, :, :]
 		out = self.output(out)
+		#out = out.transpose(0, 1)
+
+		out_shape = out.shape
 
 		return out
 
-pbp = PlayByPlayDataset('cleaned_data.csv', context_len=4)
-gs = GameSimulator(len(pbp.teams), len(pbp.plays), pbp._max_len).to('cuda:0')
-batch_size=32
-loader = DataLoader(pbp, batch_size=batch_size, collate_fn=data_collate)
+	def simulate(self, home, away, dataset, maxlen=10):
+		plays = [f'Home: {home}', f'Away: {away}']
+		teams_tens = dataset._teams_to_tensor(home, away)
 
-play_loss_fn = CrossEntropyLoss(reduction='mean')
-opt = Adam(gs.parameters())
+		for _ in range(maxlen):
+			subgame = pd.DataFrame({"description": plays})
+			plays_tens = dataset._subgame_to_tensor(subgame)
+			with torch.no_grad():
+				next_play_dist = F.softmax(self.forward(teams_tens.unsqueeze(0).unsqueeze(0), plays_tens.unsqueeze(1))[-1, :].squeeze()).numpy()
+	
+			next_play_idx = np.random.choice(self._n_plays, p=next_play_dist)
+			next_play = dataset.plays[next_play_idx]
 
-gs.to('cuda:0')
-losses = []
-epoch_prog = tqdm(range(10), desc=" epoch", position=0)
-#prog = tqdm(enumerate(loader), total=len(pbp) / batch_size, position=1, leave=False)
-for epoch in epoch_prog:#range(10):#epoch_prog:
-	for idx, (team_X, play_X, Y) in enumerate(loader):#prog:
-		team_X = team_X.to('cuda:0')
-		play_X = play_X.to('cuda:0')
-		play_X = pad_packed_sequence(play_X)[0].float()
+			plays.append(next_play)
+
+		return plays
 
 
-		Y = Y.to('cuda:0')
+if __name__ == '__main__':
+	try:
+		pbp = PlayByPlayDataset('cleaned_data.csv', min_len=3, max_len=5)
+		gs = GameSimulator(len(pbp.teams), len(pbp.plays)).to('cuda:0')
+		try:
+			gs.load_state_dict(torch.load('model.ptch'))
+			print(f'Successfully loaded model')
+		except:
+			print('Could not resume model training')
 
-		Y_pred = gs(team_X, play_X).to('cuda:0')
+		finally:
+			gs.to('cuda:0')
+			pass
+		batch_size=32
+		loader = DataLoader(pbp, batch_size=batch_size, collate_fn=data_collate, shuffle=True)
 
-		loss = play_loss_fn(Y_pred, Y)
-		losses.append(loss.item())
-		writer.add_scalar('Loss/train', loss, epoch * (len(pbp)/batch_size) + idx)
+		play_loss_fn = CrossEntropyLoss(reduction='mean')
+		opt = Adam(gs.parameters())
 
-		opt.zero_grad()
-		loss.backward()
-		opt.step()
-	epoch_prog.set_description(f'Loss: {np.mean(losses)}')
-	losses = []
+		losses = []
+		epoch_prog = tqdm(range(10), position=0)
+		print(f'Length of dataset: {len(pbp)}')
+		for epoch in epoch_prog:
+			if len(losses) != 0:
+				epoch_prog.set_postfix({'Previous epoch loss': np.mean(losses)})
+			losses = []
+			prog = tqdm(enumerate(loader), total=len(pbp) / batch_size, position=1, leave=False)
+			for idx, (team_X, play_X, Y) in prog:
+				team_X = team_X.to('cuda:0')
+				play_X = play_X.to('cuda:0')
+				play_X = pad_packed_sequence(play_X)[0].float()
+	
+				Y = pad_packed_sequence(Y)[0].float()
+				Y = Y.transpose(0, 1).transpose(1,2)
+				Y = Y.to('cuda:0')
+
+				Y_pred = gs(team_X, play_X).to('cuda:0').transpose(0,2).transpose(0,1)
+				loss = play_loss_fn(Y_pred, Y)
+				losses.append(loss.item())
+				writer.add_scalar('Loss/train', loss, epoch * (len(pbp)/batch_size) + idx)
+
+				opt.zero_grad()
+				loss.backward()
+				opt.step()
+				#epoch_prog.set_description(f'Step {idx}/{len(loader)}')
+				if (idx % 1000) == 0:
+					gs = gs.to('cpu')
+					sample = "\n".join(gs.simulate("Purdue", "UConn", pbp))
+					writer.add_text('sample', sample, idx)
+					gs = gs.to('cuda:0')
+
+			torch.save(gs.state_dict(), 'model.ptch')
+	except:
+		raise
+		#embed()
--- pbp_dataset.py
+++ pbp_dataset.py
@@ -5,11 +5,12 @@ import math
 from torch.utils.data import Dataset
 
 class PlayByPlayDataset(Dataset):
-	def __init__(self, df_path, context_len=10):
+	def __init__(self, df_path, min_len, max_len):
 		self.df = pd.read_csv('cleaned_data.csv')
 		homes = self.df['home'].unique()
 		aways = self.df['away'].unique()
 		self.teams = list(set(homes).union(set(aways)))
+		self._n_games = self.df['game_id'].nunique()
 
 		self.teams_id_map = dict([(val, idx) for idx, val in enumerate(self.teams)])
 		self.game_id_map = (
@@ -20,18 +21,87 @@ class PlayByPlayDataset(Dataset):
 			)
 		)
 
+		ranges = []
+		new_rows = []
+		for idx, game_id in enumerate(self.df.game_id.unique()):
+			game = self.df[self.df.game_id == game_id]
+			game_idx = game.index
+
+			min_idx = game_idx[0]
+			max_idx = game_idx[-1]
+			home = game.iloc[0].home
+			away = game.iloc[0].away
+
+			new_rows.append({
+				"game_id": game_id,
+				"home": home,
+				"away": away,
+				"home_score": 0,
+				"away_score": 0,
+				"description": f"Home: {home}",
+				"time_elapsed": -0.1
+			})
+
+			new_rows.append({
+				"game_id": game_id,
+				"home": home,
+				"away": away,
+				"home_score": 0,
+				"away_score": 0,
+				"description": f"Away: {away}",
+				"time_elapsed": -0.09
+			})
+
+			new_rows.append({
+				"game_id": game_id,
+				"home": home,
+				"away": away,
+				"home_score": self.df.iloc[max_idx].home_score,
+				"away_score": self.df.iloc[max_idx].away_score,
+				"description": f"END",
+				"time_elapsed": self.df.iloc[max_idx].time_elapsed + 0.1
+			})
+
+
+			self._min_len = min_len
+			self._max_len = max_len
+
+		new_stuff = pd.DataFrame(new_rows)
+		self.df = pd.concat([self.df, new_stuff])
+		self.df.sort_values(by=['game_id', 'time_elapsed'])
+		self.df = self.df.reindex()
+
 		self.plays = list(self.df['description'].unique())
 		self.plays.insert(0, '<PAD>')
 		self.play_ids_map = dict([(val, idx) for idx, val in enumerate(self.plays)])
 
-		self._max_len = context_len#self.df.game_id.value_counts().max()
-		self._n_games = self.df['game_id'].nunique()
+		self._ranges = self._generate_ranges(min_len, max_len)
+
+	def _generate_ranges(self, min_len, max_len):
+		start = 0
+		end = np.random.randint(min_len, max_len)
+
+		ranges = [(start, end)]
+
+		while end < (len(self.df) - max_len):
+			start = end
+			end = start + np.random.randint(min_len, max_len)
+
+			while self.df.iloc[end].game_id != self.df.iloc[end].game_id:
+				end -= 1
+
+			if (end - start) < 1:
+				continue
+
+			ranges.append((start, end))
+
+		return ranges
 
 	def _team_ids_to_tensor(self, team_ids):
 		tens = torch.zeros(len(self.teams))
 		tens[team_ids[0]] = 1.
 		tens[team_ids[1]] = 1.
-		return tens#.expand(self._max_len, len(self.teams))
+		return tens
 
 	def _teams_to_tensor(self, home, away):
 		home_id = self.teams_id_map[home]
@@ -39,60 +109,41 @@ class PlayByPlayDataset(Dataset):
 
 		return self._team_ids_to_tensor([home_id, away_id])
 
-	def _game_idx_and_len(self, idx:int):
-		game_idx = idx % self._n_games
-		sublen = math.floor(idx / self._n_games)
+	def __len__(self):
+		return len(self._ranges)
 
-		return game_idx, max(sublen, 4)
+	def _X_Y(self, subgame):
+		#Y = torch.tensor([self.play_ids_map[subgame.iloc[-1].description]])
+		X = torch.zeros(len(subgame)-1, len(self.plays))
+		Y = torch.zeros(len(subgame)-1, len(self.plays))
 
-	def _Y_tensor(self, game):
-		play_idx = self.play_ids_map[game.iloc[-1].description]
-		play_tens = torch.tensor([play_idx])
+		for X_idx, (_, play) in enumerate(subgame.iterrows()):
+			if X_idx != (len(subgame) - 1):
+				idx = self.play_ids_map[play.description]
+				X[X_idx, idx] = 1.
 
-		return play_tens
-
-	def _get_start_and_end(self, idx):
-		start_idx = idx
-
-		start_game_id = self.df.iloc[idx].game_id
-		end_idx = idx + self._max_len + 1
-
-		while self.df.iloc[end_idx].game_id != start_game_id:
-			end_idx -= 1
-
-		return start_idx, end_idx
-
-	def _X_tensor(self, game):
-		plays = []
-
-		play_tens = torch.zeros(self._max_len, len(self.plays), dtype=torch.long)
-		for idx, play in enumerate(plays):
-			if idx >= self._max_len:
-				break
-			play_idx = self.play_ids_map[play]
-			play_tens[idx, play_idx] = 1.
-
-		return play_tens
-
-	def _plays_tensors(self, game, game_len):
-		X = self._X_tensor(game, game_len)
-		Y = self._Y_tensor(game)
+			if X_idx != 0:
+				idx = self.play_ids_map[play.description]
+				Y[X_idx-1, idx] = 1.
 
 		return X, Y
 
-	def __len__(self):
-		return self.df.game_id.nunique() * self._max_len
+	def _subgame_to_tensor(self, subgame):
+		X = torch.zeros(len(subgame), len(self.plays))
+		for X_idx, (_, play) in enumerate(subgame.iterrows()):
+			idx = self.play_ids_map[play.description]
+			X[X_idx, idx] = 1.
+
+		return X
 
 	def __getitem__(self, idx):
-		game_idx, game_len = self._game_idx_and_len(idx)
-		game_id = self.game_id_map[game_idx]
+		start_idx, end_idx = self._ranges[idx]
 
-		game = self.df[self.df.game_id == game_id][:game_len+1]
+		subgame = self.df.iloc[start_idx:end_idx]
+		X_plays, Y = self._X_Y(subgame)
 
-		plays_tens, target_play_tens = self._plays_tensors(game, game_len)
-
-		teams = [game.iloc[0]['home'], game.iloc[0]['away']]
+		teams = [subgame.iloc[0]['home'], subgame.iloc[0]['away']]
 		team_ids = [self.teams_id_map[t] for t in teams]
 		teams_tens = self._team_ids_to_tensor(team_ids)
 
-		return teams_tens, plays_tens, target_play_tens
+		return teams_tens, X_plays, Y
